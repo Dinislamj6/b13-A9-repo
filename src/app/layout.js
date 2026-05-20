4ac1f24690030949711e4890821adb214db7dfc7
dinislamj6 <dinislamj6@gmail.com>
2026-05-21 01:35:56 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,13 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable}`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
